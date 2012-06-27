f4d026ca85099805841a1fe71686746453b45247
albertofem <albertofem@gmail.com>
2012-06-27 21:37:45 +0200

    Deleted repetitive data

--- src/AFM/Kanjidic/Constant/Dictionary.php
+++ src/AFM/Kanjidic/Constant/Dictionary.php
@@ -16,12 +16,12 @@ namespace AFM\Kanjidic\Constant;
  */
 class Dictionary
 {
-	const DICTIONARY_NELSON_C = "nelson_c";
-	const DICTIONARY_NELSON_N = "nelson_n";
-	const DICTIONARY_HALPERN_NJECD = "halpern_njecd";
-	const DICTIONARY_HALPERN_KKLD = "nelson_c";
-	const DICTIONARY_HEISIG = "heisig";
-	const DICTIONARY_GAKKEN = "gakken";
-	const DICTIONARY_ONEILL_NAMES = "oneill_names";
-	const DICTIONARY_ONEILL_KK = "oneill_kk";
+	const NELSON_C = "nelson_c";
+	const NELSON_N = "nelson_n";
+	const HALPERN_NJECD = "halpern_njecd";
+	const HALPERN_KKLD = "nelson_c";
+	const HEISIG = "heisig";
+	const GAKKEN = "gakken";
+	const ONEILL_NAMES = "oneill_names";
+	const ONEILL_KK = "oneill_kk";
 }
